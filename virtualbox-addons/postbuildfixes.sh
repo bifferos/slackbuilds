7cf55254646ad648ce50e7bb72d937ccf3a083e6
bifferos <bifferos@gmail.com>
2019-11-10 00:58:40 +0000

    Add to setup scripts

--- virtualbox-addons/postbuildfixes.sh
+++ virtualbox-addons/postbuildfixes.sh
@@ -11,13 +11,22 @@ rm -Rf dev/vboxguest
 # Clean up useradd, groupadd
 rm -Rf etc/*group* etc/*shadow* etc/*passwd* 
 
+mkdir -p var/lib/pkgtools/setup
 
-cat >> install/doinst.sh <<- ENDENDENDEND
-# Plagiarised straight from the virtualbox-kernel slackbuild, thanks Heinz.
-chroot . /sbin/depmod -a @KERNEL@
+cat >> var/lib/pkgtools/setup/setup.vboxadditions <<- ENDENDENDEND
+#!/bin/sh
+chroot . /sbin/depmod -a
+ENDENDENDEND
+
+chmod 755 var/lib/pkgtools/setup/setup.vboxadditions
+
+cat >> var/lib/pkgtools/setup/setup.onlyonce.vboxadditions <<- ENDENDENDEND
+#!/bin/sh
 # Pasted straight from the vbox install script, to make up for the etc/
 # removals above.
 groupadd -r -f vboxsf >/dev/null 2>&1
 useradd -d /var/run/vboxadd -g 1 -r -s /bin/false vboxadd >/dev/null 2>&1 || true
 useradd -d /var/run/vboxadd -g 1 -u 501 -o -s /bin/false vboxadd >/dev/null 2>&1 || true
 ENDENDENDEND
+
+chmod 755 var/lib/pkgtools/setup/setup.onlyonce.vboxadditions
